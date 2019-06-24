3fe7ec8555dab1ba6a36f70cdeb98ce50bcdc5bf
transreductionist <transreductionist@gmail.com>
2019-06-24 10:42:22 -0400

    Add data conditioning.

--- helpers/database.py
+++ helpers/database.py
@@ -13,15 +13,11 @@ def reset_database():
 def load_mysql(path, name):
     with open(get_file_path(path, name)) as csv_file_ptr:
         reader = csv.reader(csv_file_ptr)
-        header_mapper = get_header(reader)
+        header, header_mapper = get_header(reader)
         models = read_csv_to_model(reader, header_mapper)
         database.session.bulk_save_objects(models)
         database.session.commit()
-
-        schema = RedshiftSchema(many=True)
-        data = schema.dump(models).data
-
-        return data, header_mapper
+        return map_models_to_redshift_data(models, header_mapper)
 
 def dump_mysql(path, name, header_mapper, data):
     header = list(header_mapper.keys())
@@ -30,3 +26,20 @@ def dump_mysql(path, name, header_mapper, data):
         writer.writerow(header)
         for row in data:
             writer.writerow([getattr(row, column) for column in header ])
+
+def add_new_redshift_fields(header_mapper):
+    for field, value in header_mapper.items():
+        if value >= 9:
+            header_mapper[field] += 1
+    header_mapper['finl_sw'] = 9
+    header = [sorted_item[0] for sorted_item in sorted(header_mapper.items(), key=lambda kv: (kv[1], kv[0]))]
+    return header, header_mapper
+
+def map_models_to_redshift_data(models, header_mapper):
+    schema = RedshiftSchema()
+    redshift_header, redshift_header_mapper = add_new_redshift_fields(header_mapper)
+    schema.context = redshift_header_mapper
+    data = []
+    for model in models:
+        data.append(schema.dump(model).data)
+    return data, redshift_header
